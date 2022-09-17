4868a366ef5c0842e30a348115efa95919406d67
Fakiri <ismail.fakiri@dataimpact.io>
2022-09-18 01:34:08 +0200

    Pytest

--- demo/test_program_pytest.py
+++ demo/test_program_pytest.py
@@ -1,4 +1,5 @@
-from program import add, get_data
+from re import A
+from program import add, get_data, Account
 import pytest
 
 # Positive Test
@@ -12,7 +13,25 @@ def test_add():
 def test_string_input():
     assert add("Fakiri", 3) == "Error: Invalid input"
 
+# Testing Error
+def test_add_None():
+    with pytest.raises(TypeError):
+        add(None, None)
+
 # Fail Test
 def test_get_data():
     if not get_data(None):
         pytest.fail("No Data")
+
+# Fixture
+@pytest.fixture
+def account():
+    return Account(1000)
+
+def test_deposit(account):
+    account.deposit(500)
+    assert account.balance == 1500
+
+def test_withdraw(account):
+    account.withdraw(500)
+    assert account.balance == 500
