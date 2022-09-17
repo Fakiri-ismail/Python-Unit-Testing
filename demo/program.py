485f30d9434898d7fb580cf88763ad4464f00ca1
Fakiri <ismail.fakiri@dataimpact.io>
2022-09-17 22:55:38 +0200

    remove program.py

--- demo/program.py
+++ demo/program.py
@@ -1,2 +1,8 @@
 def add(x, y):
-    return x + y
+    if isinstance(x, str) or isinstance(y, str):
+        return "Error: Invalid input"
+    else:
+        return x + y
+
+def get_data(l):
+    return l
